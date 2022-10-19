b579f0b24ec7f522c0d0ecc73c81f9170f70aec3
menah3m <chabaokesi@outlook.com>
2022-10-19 23:13:24 +0800

    [feat]实现判断胜者的功能

--- src/index.js
+++ src/index.js
@@ -47,6 +47,11 @@ import './index.css';
         //使用.slice()生成数组的副本，而不是直接在原对象上修改
         //有利于追踪历史变化
         const squares = this.state.squares.slice()
+        
+        if (calculateWinner(squares)|| squares[i]){
+            return
+        }
+
         squares[i] = this.state.xIsNext ? 'X':'O'
         this.setState({
             squares: squares,
@@ -59,7 +64,13 @@ import './index.css';
     }
   
     render() {
-      const status = 'Next player: '+ (this.state.xIsNext ? 'X': 'O');
+        const winner = calculateWinner(this.state.squares)
+        let status
+        if (winner){
+            status = 'Winner: ' + winner
+        } else{
+            status = 'Next Player: '+ (this.state.xIsNext ? 'X' : 'O')
+        }
   
       return (
         <div>
@@ -101,6 +112,27 @@ import './index.css';
     }
   }
   
+// calculateWinner 判断胜者
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   // ========================================
   
   const root = ReactDOM.createRoot(document.getElementById("root"));
