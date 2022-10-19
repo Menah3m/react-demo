29a56788c4b627d29e7fd8514449cebb041cddc3
menah3m <chabaokesi@outlook.com>
2022-10-19 15:34:11 +0800

    [feat]实现从父组件传递value到子组件功能

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './index.css';
     render() {
       return (
         <button className="square">
-          {/* TODO */}
+          {this.props.value}
         </button>
       );
     }
@@ -17,7 +17,7 @@ import './index.css';
   
   class Board extends React.Component {
     renderSquare(i) {
-      return <Square />;
+      return <Square value={i}/>;
     }
   
     render() {
